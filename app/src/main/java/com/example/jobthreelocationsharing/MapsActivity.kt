65e11774f5c176c71d096d7ab72a3116afbb087b
tisha <tasumaer@gmail.com>
2025-11-25 14:38:50 +0600

    job 3

--- app/src/main/java/com/example/jobthreelocationsharing/MapsActivity.kt
+++ app/src/main/java/com/example/jobthreelocationsharing/MapsActivity.kt
@@ -2,6 +2,7 @@ package com.example.jobthreelocationsharing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,36 +14,41 @@ import com.example.jobthreelocationsharing.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityMapsBinding
+    private lateinit var googleMap: GoogleMap
+    private lateinit var firestoreViewModel: FirestoreViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMapsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_maps)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+        firestoreViewModel = ViewModelProvider(this).get(FirestoreViewModel::class.java)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Fetch user locations from Firestore and add markers
+        firestoreViewModel.getAllUsers { userList ->
+            for (user in userList) {
+                val userLocation = user.location
+                if (userLocation.isNotEmpty()) {
+                    val latLng = parseLocation(userLocation)
+                    val markerOptions = MarkerOptions().position(latLng).title(user.displayName)
+                    googleMap.addMarker(markerOptions)
+                }
+            }
+        }
+    }
+
+    private fun parseLocation(location: String): LatLng {
+        val latLngSplit = location.split(", ")
+        val latitude = latLngSplit[0].substringAfter("Lat: ").toDouble()
+        val longitude = latLngSplit[1].substringAfter("Long: ").toDouble()
+        return LatLng(latitude, longitude)
     }
 }
